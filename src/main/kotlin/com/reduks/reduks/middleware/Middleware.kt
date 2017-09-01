91afb0674e064341510aaa0e532392c5ae94ab01
Bloder <aptgetinstalldanielleme@gmail.com>
2017-09-01 18:44:35 -0300

    Accept action with a type in middleware

--- src/main/kotlin/com/reduks/reduks/middleware/Middleware.kt
+++ src/main/kotlin/com/reduks/reduks/middleware/Middleware.kt
@@ -4,5 +4,5 @@ import com.reduks.reduks.Action
 import com.reduks.reduks.Store
 
 interface Middleware<State> {
-    fun compose(reducer: (State, Action) -> State, store: Store<State>) : (State, Action) -> State
+    fun compose(reducer: (State, Action<State>) -> State, store: Store<State>) : (State, Action<State>) -> State
 }
