65645527f0670f7dec94b05b214b94006fb9db22
Daniel <wowmegadenis@hotmail.com>
2018-02-18 17:46:25 -0300

    Remove useless testing list attribution

--- src/main/kotlin/com/reduks/reduks/combine_reducers/CombineReducers.kt
+++ src/main/kotlin/com/reduks/reduks/combine_reducers/CombineReducers.kt
@@ -6,7 +6,7 @@ import com.reduks.reduks.Action
  * Created by bloder on 18/02/18.
  */
 
-class CombineReducers<State>(private val reducers: List<(State, Action<State>) -> State>) {
+class CombineReducers<State>(private val reducers: Array<out (State, Action<State>) -> State>) {
 
     fun combine() : (State, Action<State>) -> State = fun (initialState: State, action: Action<State>) : State {
         var state: State = initialState
@@ -18,8 +18,4 @@ class CombineReducers<State>(private val reducers: List<(State, Action<State>) -
     }
 }
 
-fun <State> Any.combineReducers(vararg reducers: (State, Action<State>) -> State) : (State, Action<State>) -> State {
-    val reducerList = mutableListOf<(State, Action<State>) -> State>()
-    reducers.forEach { reducerList.add(it) }
-    return CombineReducers(reducerList).combine()
-}
+fun <State> Any.combineReducers(vararg reducers: (State, Action<State>) -> State) : (State, Action<State>) -> State = CombineReducers(reducers).combine()
